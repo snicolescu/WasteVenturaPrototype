965a835c54ef38928623256b8d17044ca0e1a39a
Cerbu <cerbu89@gmail.com>
2023-11-04 17:43:17 +0200

    +a whole bunch of stuff

--- src/game.ts
+++ src/game.ts
@@ -1,11 +1,150 @@
+/// <reference path="./hex.ts" />
+
+class TileData
+{
+    public height : number = 0;
+    public water : number = 0;
+    public toxicity : number = 0;
+
+    static toxicTile() : TileData
+    {
+        let tile = new TileData();
+        tile.toxicity = 1;
+        return tile;
+    }
+
+    public static waterTile() : TileData
+    {
+        let tile = new TileData();
+        tile.water = 1;
+        return tile;
+    }
+
+    public static getRandomTile() : TileData
+    {
+        let funcs = [ TileData.toxicTile, TileData.waterTile ];
+        let tile = funcs[Math.floor(Math.random() * funcs.length)]();
+        tile.height = Math.floor(Math.random() * 5);
+        return tile;
+    }
+}
 
 
-window.onload = function () {
-    console.log("Loaded");
+function rgbToHex(r: number, g: number, b: number) {
+    return `#${((1 << 24) | (r << 16) | (g << 8) | b).toString(16).slice(1)}`;
+}
 
-    game.init();
+enum Lens {
+    None,
+    Height
+}
+let lens = Lens.None;
 
-    document.getElementById("clickme").onclick = () => { game.clicked(); };
+function setLens( button: HTMLInputElement)
+{
+    Object.keys(Lens).forEach( key => { if (button.value == key) { lens = Lens[key]; } });
+
+    hexmap.refreshGfx();
+}
+
+function setTileGfx( element : Element, tile : TileData)
+{
+    switch (lens) {
+        case Lens.Height:
+            element.removeAttribute("class");
+            element.setAttribute("fill", rgbToHex(tile.height * 50, tile.height * 50, tile.height * 50));
+            break;
+        case Lens.None:
+            element.removeAttribute("fill");
+
+            // type-specific style
+            if (tile.toxicity > 0) {
+                element.setAttribute("class", "toxic");
+            } else if (tile.water > 0) {
+                element.setAttribute("class", "water");
+            } else {
+                element.setAttribute("class", "land");
+            }
+
+            break;
+    }
+}
+
+function idx( q : number, r: number) : number
+{
+    return q + r * 1000;
+}
+
+class HexMap
+{
+    public layout : Layout;
+    public mapHtml: Element;
+    public tiles : { [key: number]: TileData; } = { };
+    public tileElements : { [key: number]: Element; } = { };
+    public onHexClicked : (q:number, r:number) => void;
+
+    constructor()
+    {
+        this.layout = new Layout( Layout.flat, Pt(100, 100), Pt(0,0));
+    }
+    
+    init()
+    {
+        this.mapHtml = document.getElementById("hexmap");
+
+        const radius = 5;
+        for (let x = -radius; x <= radius; x++) {
+            for (let y = -radius; y <= radius; y++) {
+                if (Math.abs(x + y) <= radius) {
+                    this.addHexElement( x, y);
+                }
+            }
+        }
+
+        this.setCamera( 0, 0, 0.5);
+    }
+
+    setCamera( q:number, y:number, scale:number)
+    {
+        this.mapHtml.setAttribute("transform", `rotate(30) translate(${q},${y}) scale(${scale})`);
+    }
+
+    getRandomIndex() : number
+    {
+        let keys = Object.keys(this.tiles);
+        let index = Math.floor(Math.random() * keys.length);
+        return parseInt(keys[index]);
+    }
+
+    addHexElement( q:number, r:number) : Element
+    {
+        let p = this.layout.getPixel( q, r);
+
+        const newElement = document.createElementNS("http://www.w3.org/2000/svg", "g");
+        const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+        polygon.setAttribute("points", "100,0 50,-87 -50,-87 -100,-0 -50,87 50,87");
+        newElement.setAttribute("transform", `translate(${p.x},${p.y})`);
+        newElement.appendChild(polygon);
+        newElement.onclick = () => { this.onHexClicked(q,r); };
+        newElement.onmouseenter = () => { newElement.classList.add("hover") };
+        newElement.onmouseleave = () => { newElement.classList.remove("hover") };
+        this.mapHtml.appendChild(newElement);
+
+        let tile = TileData.getRandomTile();
+        this.tiles[idx(q,r)] = tile;
+        setTileGfx( newElement, tile);
+        this.tileElements[idx(q,r)] = newElement;
+
+        return newElement;
+    }
+
+    refreshGfx() {
+        for (let key in this.tiles) {
+            let tile = this.tiles[key];
+            let element = this.tileElements[key];
+            setTileGfx( element, tile);
+        }
+    }
 }
 
 class Game
@@ -23,11 +162,29 @@ class Game
         this.scoreElement.textContent = "Score:" + this.clicks;
     }
 
-    clicked() {
+    clicked( q:number, r:number) {
         this.clicks++;
 
         this.scoreElement.textContent = "Score:" + this.clicks;
+
+        console.log("Clicked ", q, r);
     }
 }
 
 let game = new Game;
+let hexmap = new HexMap;
+
+window.onload = function () {
+    console.log("Loaded");
+
+    hexmap.init();
+    game.init();
+    hexmap.onHexClicked = (q:number, r:number) => { 
+
+        game.clicked(q,r);
+
+        game.scoreElement.textContent = "Height:" + hexmap.tiles[idx(q,r)].height;
+    };
+
+    //document.getElementById("clickme").onclick = () => { game.clicked(); };
+}
